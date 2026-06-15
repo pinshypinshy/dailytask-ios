817ac38a6e80f5943d645d8ca8ba4c308694345f
pinshypinshy <hrisyklit@gmail.com>
2026-06-15 19:13:08 +0900

    Initial Commit

---
+++ DailyTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyTask
+//
+//  Created by 平石悠生 on 2026/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyTask/DailyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTaskApp.swift
+//  DailyTask
+//
+//  Created by 平石悠生 on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct DailyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
